6fda79152da158979cc3f14f71d7980d944443d7
Dmytro Pro <dmytro.am@gmail.com>
2024-10-10 17:50:01 +0300

    Latest working code

--- packages/migrate-testrail/main/run.js
+++ packages/migrate-testrail/main/run.js
@@ -23,3 +23,28 @@ configureTestomatio(
 );
 
 await migrateTestCases();
+
+// todo: make it work with es6 exports, like in documentation (all the ES6 examples from documentation don't work)
+// https://docs.digitalocean.com/products/functions/reference/runtimes/node-js/#asynchronous-functions
+
+// todo: get rid of mysterious 404 errors like this
+// Error: Error: Failed to send data: 404 Not Found {"error":"Please add Jira integration for \"DH\" to Testomat.io project \"Codeception Demo\" to unlink \"DH-45\""}
+// 2024-10-10T14:35:47.444710338Z stderr:     at postToTestomatio (file:///tmp/5EbMULyR/testomatio.js:87:15)
+// 2024-10-10T14:35:47.444737051Z stderr:     at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+// 2024-10-10T14:35:47.444738136Z stderr:     at async retryRequest (file:///tmp/5EbMULyR/retrywrapper.js:10:20)
+// 2024-10-10T14:35:47.444742895Z stderr:     at async migrateTestCases (file:///tmp/5EbMULyR/migrate.js:298:15)
+// 2024-10-10T14:35:47.444743352Z stderr:     at async file:///tmp/5EbMULyR/run.js:28:1
+// 2024-10-10T14:35:47.44515231Z  stderr: Error adding ref: TypeError: Body is unusable
+// 2024-10-10T14:35:47.445155054Z stderr:     at specConsumeBody (node:internal/deps/undici/undici:5549:15)
+// 2024-10-10T14:35:47.445173344Z stderr:     at _Response.json (node:internal/deps/undici/undici:5451:18)
+// 2024-10-10T14:35:47.44517412Z  stderr:     at postToTestomatio (file:///tmp/5EbMULyR/testomatio.js:92:21)
+// 2024-10-10T14:35:47.445177167Z stderr:     at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+// 2024-10-10T14:35:47.445177613Z stderr:     at async retryRequest (file:///tmp/5EbMULyR/retrywrapper.js:10:20)
+// 2024-10-10T14:35:47.44518164Z  stderr:     at async migrateTestCases (file:///tmp/5EbMULyR/migrate.js:298:15)
+// 2024-10-10T14:35:47.44518201Z  stderr:     at async file:///tmp/5EbMULyR/run.js:28:1
+// 2024-10-10T14:35:47.491353826Z stderr
+
+// todo: rate limit cases handling for 429 errors from testrail (wait+retry-after)
+// https://support.testrail.com/hc/en-us/articles/7077175066388-Error-handling
+// 180 Requests per instance, per minute for TestRail Cloud Professional subscriptions.
+// 300 Requests per instance, per minute for TestRail Cloud Enterprise subscriptions.
